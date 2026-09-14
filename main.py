2c9329707c0bbc2d1eb20f6f815d0727d08715a1
Nguyen Cat Duc Khiem <duckhiem373@gmail.com>
2026-09-14 10:19:33 +0300

    done

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+Feed = input("Insert a number: ")
 
+if "." in Feed:
+    Value = float(Feed)
+else:
+    Value = int(Feed)
+
+Remainder = Value % 2
+
+print(f"Value is {Value}")
+print(f"The remainder is {Remainder} when {Value} is divided by 2.")
+print(f"{Value} / 2 = {Value /2} (Remainder: {Remainder})")
